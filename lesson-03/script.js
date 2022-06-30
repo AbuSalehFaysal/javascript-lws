a031f02c7865e954b66b65fd9aa011b796eb84d1
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2022-06-30 11:07:03 +0600

    js statements

---
+++ lesson-03/script.js
@@ -0,0 +1,14 @@
+// Statements - Examples
+document.getElementById("demo").innerHTML = "Hello World!";
+
+// Statements execute one after another
+
+// Use semicolon after the end of a statement
+
+// JS ignores multiple spaces
+
+// Use spaces around operator(+,-,*,/) - Good Practice
+
+// If a JS statement does not fit in one line, is is good practice to break it after an operator
+
+// JS statements can be group together in code block, inside curly braces {...}
