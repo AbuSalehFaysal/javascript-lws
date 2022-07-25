4fdd94b660befa1e079b08c3b4f921e9a7b6ca0f
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-07-26 02:41:12 +0600

    JS Syntax

---
+++ lesson-04/script.js
@@ -0,0 +1,30 @@
+// JS Syntax
+
+// JS Syntax is a set of rules, how JS programs are constructed.
+
+// There are two types of values
+// 1. Fixed Values 2. Variable Values
+
+// Fixed values are called literals(Numbers, Strings)
+// Variable values are called variables. Variables are used to store data values. We use var, let const to declare a variable. e.g. var x = 5.
+
+// JS Operators
+// 1. Arithmetic Operators (+, -, *, /)
+// 2. Assignment Operators (=)
+
+// JS Expressions
+//Expressions are combination of values, variables and operators, which computes to a value. e.g. 5*10 evaluates to 50.
+
+// JS Keywords
+// var, let, const
+
+// JS Comments
+// Comments are used to write notes in a program.
+
+// JS Identifiers
+// keywords, variables, functions, etc.
+// Case Sensitive
+
+// JS lowerCamelCase
+
+// JS uses Unicode Character Set
