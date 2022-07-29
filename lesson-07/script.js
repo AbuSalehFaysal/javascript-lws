48cc3d7e10e5b2f65a23eaa4fdfa227577839432
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-07-29 22:05:52 +0600

    lesson-07

---
+++ lesson-07/script.js
@@ -0,0 +1,17 @@
+// Let Keyword
+
+// 1. Let cannot cannot be redeclared anywhere.
+
+// 2. let provide block scope{}. Before ES6(2015), JS had only global and function scope. 
+
+// 3. let variables declared inside a block cannot be accessed outside the block.
+
+// We should not use var inside a block.
+
+// var variables can be hoisted to the top. 
+myname = "Faysal";
+var myname;
+console.log(myname);
+// The above code does not show any error because JS take a glance all the codes before executing, and identify the variable declarations and hoisted them to the top and initial an undefined default value.
+
+// let variables can be hoisted as well but it does not initial a default value unlike var variables. 
