23cf8e9cebd2f780146c7a54340397c990a10e03
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-07-30 18:51:53 +0600

    js operators

---
+++ lesson-09/script.js
@@ -0,0 +1,21 @@
+// JS Operators
+
+// assignment operator (=, +=, -=, *=, /=, %=, **=)
+
+// arithmetic operators (+, -, *, /, %, ++, --, **)
+
+// the + operator can also be used to concatenate strings. e.g. "hello" + "world" = "helloworld"
+
+// adding a string and a number will return a string. e.g. "hello" + 1 = "hello1"
+
+// comparison operators (==, ===, !=, !==, >, <, >=, <=, ?)
+
+// logical operators (&&, ||, !)
+
+// Type Operator
+
+// typeof operator (returns the type of the operand)
+
+// instanceof operator (returns true if the operand is an instance of the class)
+
+// bitwise operators (<<, >>, >>>)
