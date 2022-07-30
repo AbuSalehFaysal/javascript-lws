3c0bcf4178831c95dff72afa9f34e767f96496ee
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-07-31 01:51:23 +0600

    js data types

---
+++ lesson-11/script.js
@@ -0,0 +1,15 @@
+// JS Data Types
+
+// js has dynamic types
+
+// 1. String Data Types (Single quote inside double quote, double quote inside single quote)
+
+// 2. Number data Types
+
+// 3. Boolean data types
+
+// 4. Array Data Types
+
+// 5. Object data Types
+
+// 6. Undefined data types (Empty and undefined are not the same)
