a9a350eec4fa0760a7a270802c7e08c69cf95a7c
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-08-03 02:29:24 +0600

    template literals

---
+++ lesson-18/script.js
@@ -0,0 +1,14 @@
+// JS Template Literals
+
+let name = `I'm Abu Saleh Faysal`;
+let age = 18;
+
+// ``
+
+// Interpolation
+
+let person = `I'm ${age} years old`;
+
+// HTML Template
+
+let html = `<h1>Abu Saleh Faysal</h1>`
