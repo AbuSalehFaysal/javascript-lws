4b8e38f8a9ea9a14384d256b19c6f67cbe69af07
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-08-02 20:39:42 +0600

    js string

---
+++ lesson-15/script.js
@@ -0,0 +1,19 @@
+// JS Strings
+
+// ""
+
+// ' '
+
+let x = 'Abu Saleh "Faysal"'; // double quotes inside single quotes
+let y = "It's a string"; // single quote inside double quotes
+
+// \` == single quote
+// \" == double quote
+
+let vikings = "We are the so called \"Vikings\" from the north!" 
+
+// == check the value
+
+// === check the value and the type
+
+// we cannot compare the two objects using ==, we need to use ===
