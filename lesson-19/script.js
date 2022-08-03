98cf3106aaa322b19925bfcd5945c81201fe236c
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-08-04 01:00:40 +0600

    js numbers

---
+++ lesson-19/script.js
@@ -0,0 +1,29 @@
+// JS Numbers
+
+// Numbers can be written with or without decimals
+
+let x = 10e5; // exponential 
+
+// numbers are 64 bit and floating point number.
+
+// js can read 15 digit integer number without any problem
+
+// js can read 17 digit floating point number without any problem.
+
+// floating point number is not always accurate.
+
+// js uses + operator for both addition and concatenation.
+
+// addition of string and number will always return a string.
+
+// division and multiplication of number will always return a number.
+
+// NaN - Not a number 
+
+// isNaN - returns true if the argument is not a number
+
+// NaN is a number type data
+
+// number conversion
+let decimal = 10
+let num = decimal.toString(2) // returns "1010" Binary representation of 10
