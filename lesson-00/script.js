a15f4e8ddeb4c2ab69cd6038416caf8f2e8e6f03
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-12-01 02:13:29 +0600

    lesson-00

---
+++ lesson-00/script.js
@@ -0,0 +1,12 @@
+// Pre-requisite
+// 1. Basic HTML, CSS
+
+// This course is based on W3 Schools
+
+// How to Follow this course
+// 1. Don't skip the videos
+// 2. Maintain the order of videos one by one
+// 3. Read the description as well
+// 4. Repeat the videos if you do not understand at the first time
+// 5. Try to seek help from the community group of LWS
+// 6. Dedication, practice, and try to understand by researching.
