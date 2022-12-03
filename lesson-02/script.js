f79246265b6fdf63b1d6f5f070eaa133574230e4
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2022-12-03 23:36:46 +0600

    class

--- lesson-02/script.js
+++ lesson-02/script.js
@@ -1,19 +1,19 @@
 // JavaScript Output
 
 // By ID
-document.getElementById("demo").innerHTML = 5+6;
+document.getElementById("demo").innerHTML = 5 + 6;
 
 // By Class
-document.getElementsByClassName.innerHTML = 5+6;
+document.getElementsByClassName("demo-class").innerHTML = 5 + 6;
 
 // document.write - replace everything in the document after loading the entire page. We use it only for testing.
-document.write(5+6);
+document.write(5 + 6);
 
 // window.alert - It is already in the system, so we can avoid window if we want.
-window.alert(5+6);
+window.alert(5 + 6);
 
 // console - Use mostly for debugging purpose
 console.log("Hello World");
 
-// window.print - Open the printing interface when we want to print something.  
+// window.print - Open the printing interface when we want to print something.
 window.print();
