e726fda934afa19c26781e97c737df0b581119fb
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2022-12-03 23:23:56 +0600

    title added

--- lesson-01/script.js
+++ lesson-01/script.js
@@ -1,7 +1,9 @@
+// INTRO TO JAVASCRIPT
+
 // 00. Ways of manupulating HTML element using js
-document.getElementById("demo").innerHTML="Hello World";
-document.getElementById("demo").src="pic.jpg";
-document.getElementById("demo").style.fontSize="24 px";
+document.getElementById("demo").innerHTML = "Hello World";
+document.getElementById("demo").src = "pic.jpg";
+document.getElementById("demo").style.fontSize = "24 px";
 
 // 01. Always write script tag before the ending of the body tag. So that it load after the end of the HTML rendering. It makes the performance better and it is the best practice.
 
@@ -10,4 +12,4 @@ document.getElementById("demo").style.fontSize="24 px";
 // 03. JavaScript file extension is .js.
 
 // 04. We can include the js file like this. It is called the external way of adding js.
-<script src="script.js"></script>
+<script src="script.js"></script>;
