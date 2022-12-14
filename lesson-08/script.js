e6eb46328231ecf5417c497881592dc8ea40b999
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-12-14 23:51:29 +0600

    took notes

--- lesson-08/script.js
+++ lesson-08/script.js
@@ -2,8 +2,10 @@
 
 // const variable cannot be reassigned.
 
-// always use const when you declare an array, object, function, regular expression. 
+// const variable value should be assigned while declaring the variable.
+
+// always use const when you declare an array, object, function, regular expression.
 
 // const works in the block scope just like let.
 
-// const variables can be hoisted as well but it does not initial a default value unlike var variables. 
+// const variables can be hoisted as well but it does not initial a default value unlike var variables.
