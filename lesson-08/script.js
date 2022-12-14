6a2dddba8741bc5b4a033648b04958c67e8fbf8f
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-12-15 00:47:33 +0600

    took notes

--- lesson-08/script.js
+++ lesson-08/script.js
@@ -6,6 +6,8 @@
 
 // always use const when you declare an array, object, function, regular expression.
 
+// array, object are like a box, by storing array, objects inside a const variable, you cannot change the memory location reference(simply the box), but you can change change the value of the array, object(value of the box).
+
 // const works in the block scope just like let.
 
 // const variables can be hoisted as well but it does not initial a default value unlike var variables.
