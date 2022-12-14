291b852dcfa684dc87bcc735dd09f606bd7bbbf0
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-12-15 02:00:59 +0600

    lesson-11

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -2,18 +2,20 @@
 
 // js has dynamic types
 
-// 1. String Data Types (Single quote inside double quote, double quote inside single quote)
+// 1. String Data Types (Single quote inside double quote, double quote inside single quote) --- "Abu Saleh Faysal", "Abeer"
 
-// 2. Number data Types
+// 2. Number data Types --- 10,1
 
-// 3. Boolean data types
+// 3. Boolean data types --- true, false
 
-// 4. Array Data Types
+// 4. Array Data Types --- [0,5,10,15,20]
 
-// 5. Object data Types
+// 5. Object data Types --- {name: "Faysal", age: 25}
 
-// 6. Undefined data types (Empty and undefined are not the same)
+// 6. Undefined data types (Empty and undefined are not the same) --- let name; declared but not defined.
 
 // While working with string and number, string will get more priority.
 
 // js evaluates any expression from left to right.
+
+// using typeOf operator, we can find out the data type of any variable.
