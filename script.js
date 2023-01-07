a959a7ca1e3e22a71af0b511cc5941808143bc83
DESKTOP-VGQL2VE\Admin <asfaysal.bracu@gmail.com>
2023-01-07 11:36:51 +0600

    functions

---
+++ script.js
@@ -0,0 +1,16 @@
+// js functions
+
+function sleep(names, time) {
+  console.log(names, " is sleeping at ", time);
+}
+
+// names, time are the parameter declared inside the parenthesis.
+
+// the function will not work until we call it like this
+sleep("faysal", "10 PM");
+
+// faysal, 10 PM are the arguments.
+
+// inside the functions the arguments(the parameters) behave as local variables.
+
+// the code inside the function will only occure when something invokes(call) the function.
