d323c1381bf7316d02d09f1768788e9d6cf77f4c
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-01-11 12:27:49 +0600

    function notes

--- lesson-12/script.js
+++ lesson-12/script.js
@@ -1,32 +1,33 @@
 // JS Function
 
-// a block of code designed to perform a particular task. 
+// a block of code designed to perform a particular task.
 
-function sum (a, b) {
-    return a+b;
+function sum(a, b) {
+  return a + b;
 }
 
-// sum is the name of the function, a and b are the two parameters. 
+// sum is the name of the function, a and b are the two parameters.
 
-sum(5,10);
+sum(5, 10);
 // 5 and 10 under the parenthesis are called the arguments.
 // inside a function, the arguments behaves as variables.
 
-// This is how we can call or invoke a function. 
+// This is how we can call or invoke a function.
 // We can call or invoke a function multiple times.
 
 // inside a function, after return statement, the function will stop executing.
 
-// If a programmer forget to return something from a function, the function returns undefined by default. 
+// If a programmer forget to return something from a function, the function returns undefined by default.
 
-// variable declared inside a function, become LOCAL to the function. 
+// variable declared inside a function, become LOCAL to the function.
 
 // variables contain same name can be used in different functions.
 
 function one() {
-    let one = 1;
+  let one = 1;
+  // inside the curly braces, everything is goes under functional scope
 }
 
 function oneMore() {
-    let one = 56;
+  let one = 56;
 }
