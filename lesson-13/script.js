c15dc9e9413062fda1e850931951f2162be010c1
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-01-14 00:16:50 +0600

    js objects

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -1,27 +1,30 @@
 // JS Object
 
 const car = {
-    name: 'BMW',
-    year: 2018,
-    price: 10000,
-    color: 'black',
-    isSold: false,
-    start: function () {
-        console.log(this.color + this.name);
-    }
-}
+  name: "BMW",
+  year: 2018,
+  price: 10000,
+  color: "black",
+  isSold: false,
+  start: function () {
+    console.log(this.color + this.name);
+  },
+};
 
 // Objects are variables too, which can store different types of data together.
 
 // We should use const while declaring objects or array.
 
 console.log(car.color);
-console.log(car['name']);
+console.log(car["name"]);
 // way of accessing object data
 
 car.start();
 
+car.start; // if we access a method without a parenthesis, it will return the function definition.
+
 // function inside an object is called a method.
+// a method is function stored as a property.
 
 let x = 5; // literal syntax
 let y = new Number(5); // object syntax, it complicates the code and slow down execution process.
