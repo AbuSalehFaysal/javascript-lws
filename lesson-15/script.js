d60667e57ff38f9fb33f37e62fd90efd349ab788
DESKTOP-VGQL2VE\Admin <asfaysal.bracu@gmail.com>
2023-01-14 22:41:00 +0600

    js string

--- lesson-15/script.js
+++ lesson-15/script.js
@@ -4,16 +4,23 @@
 
 // ' '
 
+let lengthX = "Faysal";
+console.log(lengthX.length); // return the length of the string.
+
 let x = 'Abu Saleh "Faysal"'; // double quotes inside single quotes
 let y = "It's a string"; // single quote inside double quotes
+
+// \' or \" will act as an escape character and ignore the next char.
 
 // \` == single quote
 // \" == double quote
 
-let vikings = "We are the so called \"Vikings\" from the north!" 
+let vikings = 'We are the so called "Vikings" from the north!';
 
 // == check the value
 
 // === check the value and the type
 
-// we cannot compare the two objects using ==, we need to use ===
+// we cannot compare the two objects using == or ===, it will return false always
+
+// six other escape character are valid in javascript. Such as /b, /n
