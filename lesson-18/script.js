f697a0de4e645e74cc2adb0f1e7cad5974fb1bab
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-01-18 02:18:00 +0600

    string literals

--- lesson-18/script.js
+++ lesson-18/script.js
@@ -1,14 +1,17 @@
 // JS Template Literals
 
-let name = `I'm Abu Saleh Faysal`;
+let name = `I'm Abu Saleh Faysal`; // using template literal, I do not have to worry about single or double quotes.
+
+let multiLine = `Abu
+                Saleh
+                Faysal`; // Multiline
 let age = 18;
 
 // ``
 
 // Interpolation
-
 let person = `I'm ${age} years old`;
 
 // HTML Template
 
-let html = `<h1>Abu Saleh Faysal</h1>`
+let html = `<h1>Abu Saleh Faysal</h1>`;
