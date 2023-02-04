75fe0ab9829053f2307c54e721f07ff0d15582bd
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-04 16:47:53 +0600

    js date formats

---
+++ lesson-27-js-date-formats/script.js
@@ -0,0 +1,6 @@
+// Date Formats
+
+// ISO format is the standard format for all browser. Other format may vary.
+
+// Date.parse() to get the millisecond.
+// new Date(millisecond) // get the actual date.
