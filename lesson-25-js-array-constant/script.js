b806e070a63748aa81b9a31beb7d40f75f434e4a
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-04 13:34:20 +0600

    js array constant

---
+++ lesson-25-js-array-constant/script.js
@@ -0,0 +1,12 @@
+// JS array constant
+// from ES6, it is introduced that array should be declared using const instead of var or let.
+
+const arr = ["Abu", "Saleh", "Faysal"];
+
+// array is a kind of a box, the box cannot be change but we can change the value of the box. That is why we should use const while declaring array or objects.
+
+// hoisting is not possible in const unlike var.
+
+// const works for block scope
+
+// redeclare an array using const is not allowed
