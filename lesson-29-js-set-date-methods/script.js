f9bc6e1212f2b92d27be5b232c8c8a1c3de8d778
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-05 08:57:24 +0600

    js set date methods

---
+++ lesson-29-js-set-date-methods/script.js
@@ -0,0 +1,9 @@
+// JS Set Date Methods
+
+const d = new Date();
+d.setFullYear(2020); // set the year to 2020
+d.setFullYear(2020, 11, 10); // we can also set the month and day as well in setFullYear method.
+d.setMonth(11); // Set month to december, because starts from 0.
+d.setDate(); // set the date
+d.setHours(); // set the hours
+d.setMinutes(); // set the minutes
