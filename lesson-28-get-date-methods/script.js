1eff7f1e1227b3f5bc37faf127b0c3ee32fe8446
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-05 08:40:24 +0600

    js get date methods

---
+++ lesson-28-get-date-methods/script.js
@@ -0,0 +1,11 @@
+// JS Get date Methods
+
+const d = new Date();
+
+d.getTime(); // get the time from 1970.
+d.getFullYear(); // get the year.
+d.getMonth() + 1; // get the month and we need to add 1 because month starts from 0.
+d.getDate(); // get the returns
+d.getHours() + 1; // get the hours and add 1 because hour also start from 0.
+d.getMilliseconds(); // get the milliseconds
+d.getDay(); // get the day and day starts from Sunday.
