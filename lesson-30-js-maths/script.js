ffe5062faa0920b853f323ad1044ec15b32d74b5
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-05 09:23:51 +0600

    js maths

---
+++ lesson-30-js-maths/script.js
@@ -0,0 +1,25 @@
+// JS Maths
+
+// unlike the other object, the math Object does not have any constructor
+
+// The Syntax for Math Property looks like
+Math.PI; // returns PI
+
+// Number to Integer
+// There are 4 common methods to round a number to an integer
+Math.round(x); // nearest integer.
+Math.ceil(x); // up to nearest integer.
+Math.floor(x); // down to nearest integer.
+Math.trunc(x); // returns the integer part only. 4.3 will return 4.
+
+Math.sign(-4); // return -1(Negative),0(Zero),1(Positive)
+Math.pow(8, 2); // 8 to the power 2 = 64
+Math.sqrt(64); // 8
+Math.abs(-4.7); // 4.7
+Math.sin((90 * Math.PI) / 180); // takes radian parameter
+Math.max(10, 20, 100, 5, 4); // 100
+Math.min(10, 20, 100, 5, 4); // 4
+Math.random(); // return a random number between 0 to 1
+
+// JS Random
+Math.floor(Math.random() * 10); // 0 to 9
