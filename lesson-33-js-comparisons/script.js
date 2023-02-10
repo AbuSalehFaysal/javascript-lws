0307a863e6dd0059bf4835fbc1f7dd4f2ec1a9f7
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-10 20:14:00 +0600

    lesson 33 js comparisons

---
+++ lesson-33-js-comparisons/script.js
@@ -0,0 +1,21 @@
+// JS comparisons
+
+// == (compare the values only)
+// === (compare the values and datatypes)
+
+// != (not equal value)
+// !== (not equal value or datatype)
+
+// > (greater than)
+// < (less than)
+// >= (greater than or equal)
+// <= (less than or equal)
+
+// Logical operator
+// && (AND operator)
+// || (OR operator)
+// ! (Not operator)
+
+// ternary operator
+let age = 25;
+let xyz = age >= 25 ? "Dead!" : "Not Yet!";
