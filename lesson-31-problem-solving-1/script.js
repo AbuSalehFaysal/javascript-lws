88aecfdbb68dcbc125cd8182068b219b0cbd7bb2
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-10 11:03:51 +0600

    random number

---
+++ lesson-31-problem-solving-1/script.js
@@ -0,0 +1,7 @@
+// Problem Solving
+
+// Get a random number between two numbers (both inclusive)
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+};
+console.log(getRandomNumber(1, 6));
