13e0dbc04c9b0331fd4134def0d6d237d2c15f1b
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-10 18:29:31 +0600

    lesson-32 js booleans

---
+++ lesson-32-js-booleans/script.js
@@ -0,0 +1,30 @@
+// JS Booleans
+
+// boolean represents one of two values: true or false
+
+Boolean(10 > 9); // return true
+
+// True Values
+100;
+3.14 - 15;
+("Hello");
+("False");
+7 + 1;
+
+// False Values
+let x = 0;
+let y = -0;
+let z = "";
+let a; // undefined
+let b = null;
+let c = false;
+let d = NaN;
+
+// Boolean can be objects as well
+let e = new Boolean(false); // data type object
+let f = new Boolean(false); // data type object
+
+// c == e return true
+// c === e return false because c (datatype boolean) and e (datatype object)
+
+// e == f return false because we cannot compare array or objects
