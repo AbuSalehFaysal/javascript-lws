7288bcc3f798d7190e60e640548d022594bdc086
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-11 23:07:45 +0600

    js iterables

---
+++ lesson-40-js-iterables/script.js
@@ -0,0 +1,7 @@
+// JS Iterables (array, strings, maps, NodeLists, Sets) --- basically it means we can run iteration using these(Order is important here).
+
+// iterating means looping over a sequence of elements ***Interview***
+
+// for iterables, we should use for of
+
+// Set, in set only unique elements are stored.
