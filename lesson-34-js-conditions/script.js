18d7779700384b94f2bc1ba4627e76266e952edc
DESKTOP-VGQL2VE\Admin <asfaysal.bracu@gmail.com>
2023-02-11 20:12:27 +0600

    js conditions

---
+++ lesson-34-js-conditions/script.js
@@ -0,0 +1,30 @@
+// JS Conditions
+
+// if, else if, else, switch
+
+if (condition) {
+  // do something
+} else if (condition2) {
+  // do something 2
+} else {
+  // do something else
+}
+
+switch (expression) {
+  case x:
+    // code block
+    break;
+
+  case y:
+    // code block
+    break;
+
+  default:
+  // code block
+}
+
+// the main difference between if else and switch is, in switch case if one condition meets, the code can break the loop and stop checking all the conditions.
+
+// when there are a lot of conditions, it is better to use switch case.
+
+// switch case uses the strict comparison
