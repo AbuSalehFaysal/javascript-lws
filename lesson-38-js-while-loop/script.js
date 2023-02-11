08ec67b2605b634d2709c6cac87fb179acfc0f31
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-02-11 22:53:48 +0600

    js while loop

---
+++ lesson-38-js-while-loop/script.js
@@ -0,0 +1,17 @@
+// JS While loop
+
+// it is just like the for loop. When the first statement and the last statement of for loop are optional, we can use while loop there.
+
+let i = 0;
+
+// while
+while (i < 10) {
+  console.log(i); // 0,1,2,3,4,5,6,7,8,9
+  i++;
+}
+
+// do-while
+do {
+  console.log(i); // 0,1,2,3,4,5,6,7,8,9
+  i++;
+} while (i < 10);
