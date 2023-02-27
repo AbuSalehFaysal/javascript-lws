2b34cd35754e590cf961fb9f91c57ef5aca977e9
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-02-27 23:10:36 +0600

    bitwise operations

---
+++ lesson-45-js-bitwise-operator/script.js
@@ -0,0 +1,16 @@
+// JS Bitwise Operations
+
+// operation between the binary systems are called bitwise operations
+
+// binary conversion
+(3).toString(2); // return 11
+
+// 3 & 5 // 011 & 101 // 001
+
+// << left Shift
+// >> Right Shift
+// >>> Right(Zero Filled) Shift
+
+// decimal to Binary
+// 0b indicates that the number is a binary number
+(0b110000).toString(10); // 48
