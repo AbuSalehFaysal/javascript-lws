24443e888426885d66b0fa57640853f1c1cffa63
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-04-07 17:50:10 +0600

    js strict mode

---
+++ lesson-50-js-strict-mode/script.js
@@ -0,0 +1,11 @@
+// JS Strict Mode
+
+"use strict";
+
+// It is not a statement but a literal expression comes from ES5
+
+// You can use strict mode in all your programs, it helps you to write cleaner code. For example you cannot assign a value to a variable without declaring it.
+
+// strict mode converts bad syntax into errors
+
+// reserved keywords cannot be used as variable names in strict mode
