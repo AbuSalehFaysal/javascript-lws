b9bf320d709b8f3ed6db5470c4964706773ebb40
DESKTOP-VGQL2VE\Admin <asfaysal.bracu@gmail.com>
2023-04-24 03:26:20 +0600

    js style guide

---
+++ lesson-56-js-style-guide/script.js
@@ -0,0 +1,10 @@
+// JS style guide
+
+// variable name - camelCase
+// variable name - start with letter
+
+// indentation
+
+// end statement with semicolon
+
+// after declaring object, end with semicolor, but not after function
