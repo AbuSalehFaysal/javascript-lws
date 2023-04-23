d7627cf898e5bbd405cc95f7b775c6398fc19756
DESKTOP-VGQL2VE\Admin <asfaysal.bracu@gmail.com>
2023-04-23 17:01:30 +0600

    js debugging

---
+++ lesson-55-js-debugging/script.js
@@ -0,0 +1,10 @@
+// JS Debugging
+
+// error find and solve
+
+// code might contain syntax and logical errors
+
+// way of debugging
+// 1. debugging tool - set break points
+// 2. console.log()
+// 3. debugger keyword
