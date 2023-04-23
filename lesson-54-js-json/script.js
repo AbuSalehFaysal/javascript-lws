d4580a0703825d6b3b3a6449e638ea66330faec5
DESKTOP-VGQL2VE\Admin <asfaysal.bracu@gmail.com>
2023-04-23 14:55:51 +0600

    js json

---
+++ lesson-54-js-json/script.js
@@ -0,0 +1,20 @@
+// JS Json
+
+// JSON - JavaScript Object Notation
+
+// JSON is a data format for storing and transporting data.
+
+// JSON is a lightweight data interchange format
+// it is language independent - we can use it in any programming language
+
+// JSON is derived from JS Object notation format but it is text only
+
+// JSON Syntax Rules:
+// 1. name/value pairs
+// 2. data is separated by commas,
+// 3. curly braces holds object
+// 4. square brackets holds array
+
+// We can convert JSON text to valid JS object by using JSON.parse().
+
+// We can convert a JS object to a JSON formate text by using JSON.stringify()
