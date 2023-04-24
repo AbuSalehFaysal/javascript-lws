89802ae4e80393757bb7048a573398abdac8e426
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-04-24 16:02:01 +0600

    js style guide

--- lesson-56-js-style-guide/script.js
+++ lesson-56-js-style-guide/script.js
@@ -1,10 +1,15 @@
 // JS style guide
 
 // variable name - camelCase
+// Global Variable - UPPERCASE
 // variable name - start with letter
 
 // indentation
 
 // end statement with semicolon
 
-// after declaring object, end with semicolor, but not after function
+// after declaring object, end with semicolon, but not after function or for loops or any other complex statements
+
+// for readability, avoid lines length more than 80 characters
+
+// Should not use $ sign before variable name
