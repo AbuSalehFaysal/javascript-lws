09625a0d9d02f380aa2e360aac7dbce7975da8b4
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-04-25 02:11:53 +0600

    js best practices

---
+++ lesson-57-js-best-practices/script.js
@@ -0,0 +1,11 @@
+// JS Best Practices
+
+// 1. Avoid Global variables.
+// 2. avoid new syntax like new String() and use literal syntax let x = 0;
+// 3. avoid double equal == and use === instead.
+// 4. avoid eval() function which is used to run text as code, because it can generate security problems.
+// 5. always use local variables.
+// 6. always declare variable first and then use it.
+// 7. declare arrays or objects using const.
+// 8. use default parameters.
+// 9. always end your switch case using default case.
