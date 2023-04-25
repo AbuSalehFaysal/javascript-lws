5d5c148815e6202d56e8c46fc5edfc6f99a766c5
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-04-25 20:49:01 +0600

    js common mistakes

---
+++ lesson-58-js-common-mistakes/script.js
@@ -0,0 +1,7 @@
+// JS Common mistakes
+
+// assignment operator =
+// conditional double equal operator ==
+
+// sometimes adding two floating numbers, we do not get the expected answers, because js takes 64 bit floating number
+// to avoid this problem we need to convert the floats to decimals by multiplying it using 10 and then divide it by 10
