5d62eca11af9a8f19cc85e1ee7410830869aaee7
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-12-09 23:43:31 -0330

    js object maps

---
+++ lesson-71-js-object-maps/script.js
@@ -0,0 +1,31 @@
+// JS Object Maps
+
+// Map hold key value pair just like objects
+// map can hold data types as key, whereas Object can hold string as key
+
+const person = {
+  name: "Abu Saleh Faysal",
+  age: 25,
+};
+
+let pName = person["name"]; // name is string
+
+// for map the order of the keys are important unlike objects
+// maps has a size property, but object does not have any size property
+
+// ways of creating map --- new Map ([10, 20, 30])
+
+// map.sep to add key-value or edit-update key-value
+
+// map.get(key) // return value
+// map.delete(key) // delete targeted key-value
+
+// map.clear() // to remove all the elements from map
+
+// map.has // check the targeted element is there or not
+
+// typeOf map // objects
+
+// instanceOf map // map
+
+// Object Vs Map // ***Interview***
